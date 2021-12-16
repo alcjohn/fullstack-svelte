c97c089c2b8e728dcaa0161ebd4ea97dd8bfb9ad
alcjohn <john.bioux@gmail.com>
2021-12-16 22:42:19 +0000

    gitpod config

--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,10 @@ const config = {
 		target: '#svelte',
 		vite: {
 			server: {
-				hmr: process.env.HMR_HOST ? 443 : 3000,
-				host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://") : "localhost"
+				hmr: {
+					clientPort: process.env.HMR_HOST ? 443: 3000,
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+				}
 			}
 		}
 	}
